187d67b8a552d45a10277a92945a49a33f3ca396
lijiamao <951572198@qq.com>
2026-02-09 17:29:06 +0800

    优化全局

--- src/main.js
+++ src/main.js
@@ -4,17 +4,9 @@ import './style.less'
 import App from './App.vue'
 import BasicButton from './components/BasicButton.vue'
 
-// 设置应用标题
-document.title = import.meta.env.VITE_APP_TITLE || 'Dark Room'
-
 const app = createApp(App)
 const pinia = createPinia()
 app.use(pinia)
 // 注册全局组件
 app.component('BasicButton', BasicButton)
-
-// 暴露gameStore到全局，方便ControlContainer组件访问
-import { useGameStore } from './stores/gameStore'
-window.gameStore = useGameStore()
-
 app.mount('#app')
